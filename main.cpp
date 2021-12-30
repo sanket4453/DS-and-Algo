75cdb503c984dade6a7bda4c1337468df837facc
Sanket Pawar <66305701+sanket4453@users.noreply.github.com>
2021-12-30 07:53:04 +0000

    variables intro

--- main.cpp
+++ main.cpp
@@ -3,5 +3,31 @@
 using namespace std;
 
 int main(){
-  cout << "Hello World" << endl;
+  // cout << "This is second program" <<endl;
+  int a = 123;
+  cout << a << endl;
+
+  char ch = 'b';
+  cout << ch << endl;
+
+  bool b1 = true;
+  cout << b1 << endl;
+
+  float f = 1.3;
+  cout << f << endl;
+
+  double d = 2.32;
+  cout << d << endl;
+
+  int s1 = sizeof(a);
+  int s2 = sizeof(ch);
+  int s3 = sizeof(b1);
+  int s4 = sizeof(f);
+  int s5 = sizeof(d);
+   cout << s1 << endl;
+    cout << s2 << endl;
+     cout << s3 << endl;
+      cout << s4 << endl;
+       cout << s5 << endl;
+
 }
