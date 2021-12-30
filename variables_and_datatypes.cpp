3c26191ecbdaa88732e149451d21625472653e1f
sanket4453 <66305701+sanket4453@users.noreply.github.com>
2021-12-30 16:27:12 +0530

    Create variables_and_datatypes.cpp

---
+++ variables_and_datatypes.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+  // cout << "This is second program" <<endl;
+  // Intro About Data types and variables
+ /* int a = 123;
+  cout << a << endl;
+
+  char ch = 'b';
+  cout << ch << endl;
+
+  bool b1 = true;
+  cout << b1 << endl;
+
+  float f = 1.3;
+  cout << f << endl;
+
+  double d = 2.32;
+  cout << d << endl;
+
+  int s1 = sizeof(a);
+  int s2 = sizeof(ch);
+  int s3 = sizeof(b1);
+  int s4 = sizeof(f);
+  int s5 = sizeof(d);
+   cout << s1 << endl;
+    cout << s2 << endl;
+     cout << s3 << endl;
+      cout << s4 << endl;
+       cout << s5 << endl; */
+
+// Intro about Operaters
+int a = 2;
+int b = 3;
+
+int first = a==b;
+cout << first << endl;
+
+int second = a<b;
+cout << second << endl;
+
+int third = a>b;
+cout << third << endl;
+
+int four = a<=b;
+cout << four << endl;
+
+int fifth = a>=b;
+cout << fifth << endl;
+
+int six = a!=b;
+cout << six << endl;
+
+// Some more operators are also availabe && || !
+}
